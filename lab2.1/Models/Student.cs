a1b88ba054849728c5726b8c33fb953e3ed67f6d
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-12-23 09:28:20 +0200

    created project

---
+++ lab2.1/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace lab2._1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
